a8b68956c3b05e9acf1d06145fc6cac92c1e9d09
hugoresende27 <Hugoresende27@gmail.com>
2021-09-07 14:17:18 +0100

    last

--- Modulo 2 Guanabara/ex54_maioridade_contador.py
+++ Modulo 2 Guanabara/ex54_maioridade_contador.py
@@ -1,16 +1,23 @@
 from datetime import date
-ano_a=date.today().year
+ano_a=date.today().year     #pegar o ano atual na var ano_a, usando a lib datetime e importando apenas a date
 
 listaM=[]
 listam=[]
+
+contadorM=0
+contadorm=0
 
 for x in range (1,8):
     ano_nasc=int(input("ANO NASCIMENTO PESSOA NR {} --> ".format(x)))
     idade=ano_a-ano_nasc
     if (idade>=18):
         listaM.append(idade)
+        contadorM+=1
     else:
         listam.append(idade)
+        contadorm+=1
 print ("Lista MAIORES:: {}".format(listaM))
 print ("Lista menores:: {}".format(listam))
 print ("Existem {} MAIORES e {} menores".format(len(listaM),len(listam)))
+
+print ("Com contadores, existem {} MAIORES ou {} menores".format(contadorM,contadorm))
--- Modulo 2 Guanabara/ex55_maior_e_menor_peso5pessoas.py
+++ Modulo 2 Guanabara/ex55_maior_e_menor_peso5pessoas.py
@@ -1,7 +1,20 @@
 pesos=[]
+pesomin=0
+pesomax=0
 
 for x in range (1,6):
     peso=float(input("PESO DA PESSOA {}-->".format(x)))
     pesos.append(peso)
+    if x==1:            #na primeira iteracao o pesomax e pesomin vai se o primeiro (peso)
+        pesomax=peso        #primeira iteracao, pesomax recebe peso e pesomin recebe peso tbm
+        pesomin=peso
+    else:
+        if peso>pesomax:
+            pesomax=peso
+        if peso<pesomin:
+            pesomin=peso
 
 print ("O MAIOR peso é {}Kg e o menor é {}Kg".format(max(pesos),min(pesos)))
+
+print ("peso minimo {}kg".format(pesomin))
+print ("peso máximo {}kg".format(pesomax))
