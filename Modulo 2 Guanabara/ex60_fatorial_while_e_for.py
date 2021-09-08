6dbe00d8b3f0f4a245c9d02915a09ee49c2093a1
hugoresende27 <Hugoresende27@gmail.com>
2021-09-08 17:30:05 +0100

    q

--- Modulo 2 Guanabara/ex60_fatorial_while_e_for.py
+++ Modulo 2 Guanabara/ex60_fatorial_while_e_for.py
@@ -4,6 +4,11 @@ while (num>1):          #enquanto o num for maior do que 1
     print ("{} X ".format(num),end="")  #print formatado
     fatorial = fatorial*num     #fatorial recebe fatorial*num
     num-=1                      #decremento da var num -1 a cada iteração
-    
+print ("=",fatorial)    
 
-print ("=",fatorial)
+
+num2=int(input("Qual o número B? --> "))
+factorial2=1
+for i in range(1,num2 + 1):                     #com ciclo for, iniciar factorial2 a 1, for de 1 a num2+1
+       factorial2 = factorial2*i                #factorial2 recebe o próprio valor X 1,2,3,4 até num2+1
+print("o valor {} tem fatorial {}".format(num2,factorial2))
