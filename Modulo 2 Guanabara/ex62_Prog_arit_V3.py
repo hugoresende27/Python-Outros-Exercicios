bd7a0984ed8fc2c70f6b9aa7f049483f667d6745
hugoresende27 <Hugoresende27@gmail.com>
2021-09-09 09:18:56 +0100

    manha

---
+++ Modulo 2 Guanabara/ex62_Prog_arit_V3.py
@@ -0,0 +1,16 @@
+print ("\033[33;7m "+"{:=^40}".format("10 PASSO DE Progressão Aritmética V3")+"\033[m")
+
+inicio=int(input("Qual o primeiro termo? --> "))
+passo=int(input("P.A.? --> "))
+fim=inicio+(passo*10)           #para calcular o fim, tenho de multiplicar o passo por 10 e somar o inicio
+
+while (inicio<fim):     #enquanto inicio menor do que fim
+    print ("\033[34m{}\033[m".format(inicio),end=" | ")     #print inicio
+    inicio+=passo                                           #inicio é igual ao inicio mais o passo a cada iteração
+
+fim2=int(input("\n\033[33mQueres mais quantos passos?? --> \033[m"))
+fim2=fim+(passo*fim2)
+while (inicio<fim2):
+    print ("\033[34m{}\033[m".format(inicio),end=" | ")     #print inicio
+    inicio+=passo 
+
