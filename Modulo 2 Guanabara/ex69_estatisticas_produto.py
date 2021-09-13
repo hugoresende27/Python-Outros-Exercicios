b85042d0d89ee385c779678cbc555fb3f7e92e14
hugoresende27 <Hugoresende27@gmail.com>
2021-09-13 11:19:42 +0100

    Update ex69_estatisticas_produto.py

--- Modulo 2 Guanabara/ex69_estatisticas_produto.py
+++ Modulo 2 Guanabara/ex69_estatisticas_produto.py
@@ -1,18 +1,24 @@
 print ("\033[32;7m "+"{:=^40}".format("ESTATISTICAS PRODUTO")+"\033[m")
 total=0
 produtos=0
-mais_barato=0
 while True:
+   
     nome=str(input("Nome do produto--> "))
+    
     preco=float(input("Preco--> "))
+    if total==0:
+        mais_barato=preco
+        nome_mais_barato=nome
+    
     total+=preco
+    if preco>=1000:
+        produtos+=1
     if preco<mais_barato:
         mais_barato=preco
         nome_mais_barato=nome
-    if preco>=1000:
-        produtos+=1
+    
 
-    sair=str(input("Quer continuar? [S/N]")).lower()[0]
+    sair=str(input("Quer continuar? [S/N]-->")).lower()[0]
     while not sair in 'sn':
         sair=str(input("Quer continuar? [S/N]--> ")).lower()[0]
     if sair=="n":
@@ -21,7 +27,7 @@ while True:
 
 print (f"Total da compra {total} Euros")
 print (f"Temos {produtos} produtos custando mais de 1000 €")
-print (f"O produto mais barato foi a {nome_mais_barato} e custou {mais_barato}")
+print (f"O produto mais barato foi a {nome_mais_barato} e custou {mais_barato} €")
 
 
 
